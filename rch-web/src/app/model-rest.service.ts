2211922e3dde8d9d6bbaf2f5ce0a1aaa9b32ccd7
Alejandro Rodríguez Lugo <arodriguez@abargon.com>
2018-04-10 17:27:50 -0500

    REST

--- rch-web/src/app/model-rest.service.ts
+++ rch-web/src/app/model-rest.service.ts
@@ -8,35 +8,62 @@ import { RouteBooking } from './model/routebooking';
 import { Travel } from './model/travel';
 import { Response } from './model/response';
 import { AvailabilityQuery } from './model/availabilityquery';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import {Observable} from "rxjs/Rx";
+import {FromJSONable} from "./model/FromJSONable";
 @Injectable()
 export class ModelRestService implements IModel {
 
   constructor(private http:HttpClient) { }
-  private base:string="http://localhost:4200";
-  private prefix:string="v1";
+  private base:string="http://10.10.32.60";
+  private prefix:string="rest/v1";
+  stdCall<T>(url:string,method:string="get",data0=null,params0={},headers0=null,it:string=null):Observable<Response<T>>{
+    let obs:Observable<Response<T>> = new Observable((observer)=>{
+      var headers = {
+        'X-Rch-At':'vXykLvyM7VmfGZLNYn3D'
+      };
+      if(headers0 != null){
+        for(var i in headers0){
+          let v = headers0[i];
+          headers[i] = v;
+        }
+      }
+      if(method == "get"){
+        this.http.get<Response<T>>(url,{'headers':headers,'params':params0}).subscribe((data)=>{
+          observer.next(data);
+        },(error)=>{observer.error(error);});
+      }else if(method == "post"){
+        this.http.post<Response<T>>(url,data0,{'headers':headers}).subscribe((data)=>{
+          observer.next(data);
+        },(error)=>{observer.error(error);});
+      }else if(method == "put"){
+        this.http.put<Response<T>>(url,data0,{'headers':headers}).subscribe((data)=>{
+          observer.next(data);
+        },(error)=>{observer.error(error);});
+      }
+    });
+    return obs;
+  }
   getRoutes():Observable<Response<Route[]>>{
-    this.http.get(this.base+"/"+this.prefix+"/routes");
-    return null;
+    return this.stdCall<Route[]>(this.base+"/"+this.prefix+"/routes");
   }
   createIntent(type:string):Observable<Response<Intent>>{
-    return null;
+    return this.stdCall<Intent>(this.base+"/"+this.prefix+"/intent/"+type,"post");
   }
   createSession():Observable<Response<SessionToken>>{
-    return null;
+    return this.stdCall<Intent>(this.base+"/"+this.prefix+"/session","post",null,{},null,"create-session");
   }
   getStatus():Observable<Response<boolean>>{
-    return null;
+    return this.stdCall<boolean>(this.base+"/"+this.prefix+"/status");
   }
   getRouteScheduleAvailable(id:number,query:AvailabilityQuery):Observable<Response<Schedule>>{
-    return null;
+    return this.stdCall<Schedule>(this.base+"/"+this.prefix+"/routes/"+id+"/schedule/available","get",null,query);
   }
   saveRouteBooking(b:RouteBooking):Observable<Response<RouteBooking>>{
-    return null;
+    return this.stdCall<RouteBooking>(this.base+"/"+this.prefix+"/routes/booking","post",b,{},null,"put-booking");
   }
   getTravel(id:number):Observable<Response<Travel>>{
-    return null;
+    return this.stdCall<Travel>(this.base+"/"+this.prefix+"/travel/"+id);
   }
 
 }
---
+++ rch-web/src/app/model/FromJSONable.ts
@@ -0,0 +1,3 @@
+export interface FromJSONable{
+    parseJSONObject(object:Object);
+}
