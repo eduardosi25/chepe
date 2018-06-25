237ad3b674389a27d2f1c95446b4bc60b119716a
echavez <echavez@abargon.com>
2018-06-25 17:43:21 -0500

    fix video component

--- rch-web/src/app/info/index/index.component.ts
+++ rch-web/src/app/info/index/index.component.ts
@@ -16,7 +16,8 @@ export class IndexComponent implements OnInit {
   scriptInit(){
     // Video player load
     $(document).ready(function () {
-      $('#bgvid').on('ended', function () {
+      $("#modal-video").append('<video poster="assets/img/bg/mountain-water.jpg" id="bgvid" autoplay="1" playsinline muted><source src="assets/img/chepe.webm" type="video/webm"><source src="assets/img/chepe.mp4" type="video/mp4"></video>');
+      $('body').delegate('ended','#bgvid', function () {
         $("#modal-video").fadeOut();
         $("body").css("overflow", "auto");
         $('#modal-promocion').modal();
@@ -27,7 +28,6 @@ export class IndexComponent implements OnInit {
       $("#modal-promocion").click(function () {
         $("body").addClass("active");
       });
-
     });
 
     $('.modal-video').click(function () {
