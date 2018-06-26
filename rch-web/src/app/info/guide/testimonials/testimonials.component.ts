52aee23a281014f2458543ea4ce5c5de29d67000
VE <vescamilla@abargon.com>
2018-06-25 19:16:45 -0500

    Ajuste de carga de Trip Advisor

--- rch-web/src/app/info/guide/testimonials/testimonials.component.ts
+++ rch-web/src/app/info/guide/testimonials/testimonials.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestimonialsComponent implements OnInit {
 
-  constructor() { 
-    
+  constructor() {
+
   }
 
   ngOnInit() {
-    
+
   }
 
 }
