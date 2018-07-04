482efbd2c6dc728fd53e2197aba8ead1a9b8bc00
Israel Izurieta <iizurieta@abargon.com>
2018-07-04 16:23:49 -0500

    Tradución Main y ajuste mapas en main

--- rch-web/src/environments/environment.ts
+++ rch-web/src/environments/environment.ts
@@ -5,9 +5,7 @@
 
 export const environment = {
   production: false,
-  // restws_base: "http://localhost:4200/rest",
-  // model:"dummy-rest",
-  restws_base: "http://10.10.32.60:8085",
-  model: "rest",
+  restws_base: "http://localhost:4200/rest",
+  model:"dummy-rest",
   avmode: 2 //Modo de envío de passenger types al solicitar disponibilidad. 1=[objetos] 2=[ids de passenger types]
 };
