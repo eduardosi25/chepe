d9c473305fe82b82a3f4df44070aa0fd10e27fe9
Benjamín Sánchez Váz <bsanchez@abargon.com>
2018-10-30 10:37:26 -0600

    Contacto de emergencia

--- rch-web/src/app/rch/step4/step4.component.ts
+++ rch-web/src/app/rch/step4/step4.component.ts
@@ -26,10 +26,8 @@ export class Step4Component implements OnInit {
     public session: SessionService,
     private router: Router) { }
     public paises: Country[] = [];
-    public per;
-    public flag;
-    public perl;
-    public flagl;
+    public personName;public personLast;public email;public cellphone;public emergency;
+    public flagName;public flagLast;public flagEmail;public flagCell;public flagEmergency;
 
   public segments: Segment[] = [];
   ngOnInit() {
@@ -51,40 +49,49 @@ export class Step4Component implements OnInit {
     this.location.back();
   }
   static email_regex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  static name_regex: RegExp = /^[a-zA-ZñÑ]{2,30}$/;
-  static lastname_regex: RegExp = /^[a-zA-ZñÑ]{2,30}$/;
-  static cellphone_regex: RegExp = /[0-9]{9,9}/;
+  static name_regex: RegExp = /^[A-Za-zÑñ\s]{2,30}$/;
+  static lastname_regex: RegExp = /^[A-Za-zÑñ\s]{2,30}$/
+  static cellphone_regex: RegExp = /^[0-9]{10,10}$/;
+  static emergency_regex: RegExp = /^[0-9]{10,10}$/;
+ 
+ 
   public readyToGoNext(): boolean {
     $('.form-control').removeClass('orange');
     for (var i = 0; i < this.session.rb.persons.length; i++) {
       let p: Person = this.session.rb.persons[i];
-      this.per = p.name
-      this.perl = p.lastname
-      this.name();
-      this.last();
-      if (!p.name || this.flag == true) { $('#p_' + i + '_name').addClass('orange'); return false; }
-      if (!p.lastname || this.flagl == true) { $('#p_' + i + '_lastname').addClass('orange'); return false; }
+      this.personName = p.name;
+      this.personLast = p.lastname;
+      
+      this.pName();
+      this.pLast();
+      
+      if (!p.name || this.flagName == true) { $('#p_' + i + '_name').addClass('orange'); return false; }
+      if (!p.lastname || this.flagLast == true) { $('#p_' + i + '_lastname').addClass('orange'); return false; }
       if(!p.country){$('#p_'+i+'_country').addClass('orange');return false;}
     }
-
+      this.email = this.session.rb.etickets_email;
+      this.cellphone = this.session.rb.etickets_phone;
+      this.emergency = this.emergency;
+  
     if (!Step4Component.email_regex.test(this.session.rb.etickets_email)) { $('#etickets_email').addClass('orange'); return false; }
     if (this.session.rb.etickets_email2 != this.session.rb.etickets_email) { $('#etickets_email2').addClass('orange'); return false; }
     if(this.session.rb.etickets_phone == "" || !Step4Component.cellphone_regex.test(this.session.rb.etickets_phone)){$('#etickets_phone').addClass('orange');return false;}
+    if(this.emergency == "" || !Step4Component.emergency_regex.test(this.emergency)){$('#emergencia').addClass('orange');return false;}
     return this.session.rb.pp;
   }
-  public name(){
-    if(typeof this.per != 'undefined'){
-    this.per= this.per.replace(/^\s+|\s+$/g, "");
-   if( !Step4Component.name_regex.test(this.per)){this.flag = true}
-   else (this.flag = null)
+  public pName(){
+    if(typeof this.personName != 'undefined'){
+    this.personName= this.personName.replace(/^\s+|\s+$/g, "");
+   if( !Step4Component.name_regex.test(this.personName)){this.flagName = true}
+   else (this.flagName = null)
     }
   }
 
-  public last(){
-    if(typeof this.perl != 'undefined'){
-    this.perl= this.perl.replace(/^\s+|\s+$/g, "");
-   if( !Step4Component.lastname_regex.test(this.perl)){this.flagl = true}
-   else (this.flagl = null)
+  public pLast(){
+    if(typeof this.personLast != 'undefined'){
+    this.personLast= this.personLast.replace(/^\s+|\s+$/g, "");
+   if( !Step4Component.lastname_regex.test(this.personLast)){this.flagLast = true}
+   else (this.flagLast = null)
     }
   }
   //  public getCountry(){
